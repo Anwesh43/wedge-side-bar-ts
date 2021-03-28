75b4f53d597eddb9833c204fe53bf4c31eb85c90
Anwesh43 <anweshthecool0@gmail.com>
2021-03-28 17:11:49 +0530

    adding State class

--- index.ts
+++ index.ts
@@ -99,3 +99,27 @@ class Stage {
         stage.handleTap()
     }
 }
+
+class State {
+
+    scale : number = 0 
+    dir : number = 0
+    prevScale : number = 0 
+
+    update(cb : Function) {
+        this.scale += scGap * this.dir 
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir 
+            this.dir = 0
+            this.prevScale = this.scale 
+            cb()
+        }
+    }
+
+    startUpdating(cb : Function) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale 
+            cb()
+        }
+    }
+}
