024c2b5d7e539a1d49ca2a4f2fcaa5395b82eaff
Anwesh43 <anweshthecool0@gmail.com>
2021-03-28 17:05:18 +0530

    adding Stage class

--- index.ts
+++ index.ts
@@ -68,3 +68,34 @@ class DrawingUtil {
         context.restore()
     }
 }
+
+class Stage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D 
+
+    initCanvas() {
+        this.canvas.width = w 
+        this.canvas.height = h 
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = backColor 
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : Stage = new Stage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}
